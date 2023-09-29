ecd0d62588f3a25bd67b912c53c28c15e0dbbf08
unknown <dranoelnoicaler@gmail.com>
2023-09-29 13:19:07 +0800

    leonard landing page

--- client/src/scenes/home/Home.js
+++ client/src/scenes/home/Home.js
@@ -42,15 +42,43 @@ const Home = () => {
       </div>
 
       <div className="inspi-q">
-        <div className="quote">
-          This is an inspiring quote, or a testimonial from a customer. Maybe
-          it's just filling up space, or maybe people will actually read it.
-          Who knows? All I know is that it looks nice.
-        </div>
-        <div className="thor-name">-Thor, God of Thunder</div>
-      </div>
+        
+        <div className="inspi-q__inside">
 
-      <div className="content-random"><br/>
+            <div className="AboutDECO">
+           <div className="quote">
+            About DECO
+        
+              </div> 
+              <div className="thor-name">
+              -Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+              ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+              commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </div>
+                </div>
+              </div>
+              <div className="border-inspi">
+              </div>
+              <div className="inside-border">
+                  <div className="number-1">
+                    100
+                    <div className="size">Properties</div>
+                  </div>
+                  <div className="number-2">
+                    99
+                    <div className="size">Families</div>
+                  </div>
+                  <div className="number-3">
+                    468
+                    <div className="size">Occupants</div>
+                  </div>
+                  <div className="number-4">
+                    789
+                    <div className="size">Lorem ipsum</div>
+                  </div>
+                </div>
+        <div className="content-random"><br/>
         <div className="random-title">Deco HOA Services</div>
         <div className="content-container">
           <div className="info-ran">
@@ -72,13 +100,17 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
-
-    
-      <div className="footer">
+        <div className="footer">
         Copyright © The Deco Homeowners' Association System by Mercado Family 2022
       </div>
     </div>
+      </div>
+      </div>
+
+      
+
+    
+      
   );
 };
 
