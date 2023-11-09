26c97842723e869f0fbde0bfc184ffacd89e0598
josephllyd <marumasjoseph@gmail.com>
2023-11-09 23:43:52 +0800

    minor additions to sprint 4, added cards

--- client/src/scenes/memberPages/profile/index.jsx
+++ client/src/scenes/memberPages/profile/index.jsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
-import { Card, CardContent, CardMedia, IconButton, InputBase } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  IconButton,
+  Dialog,
+  DialogContent,
+  Typography,
+  Menu,
+  MenuItem,
+  Popover,
+} from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { useTheme } from "@mui/material/styles";
 import { fetchUsers, handleEditUser, useUserData } from "api/usersApi";
 
@@ -8,11 +21,22 @@ const Profile = () => {
   const userData = useUserData();
   const theme = useTheme();
   const [users, setUsers] = useState([]);
-  
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   useEffect(() => {
-    fetchUsers(setUsers); 
+    fetchUsers(setUsers);
   }, []);
+
+  const handleCardClick = (event, user) => {
+    setAnchorEl(event.currentTarget);
+    setSelectedUser(user);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setSelectedUser(null);
+  };
 
   return (
     <div style={{ flex: 1, padding: "20px", fontSize: "20px" }}>
@@ -24,39 +48,59 @@ const Profile = () => {
             gap: "3rem",
             padding: "0.1rem 1.5rem",
           }}
-        >
-        </div>
+        ></div>
       </div>
       <br />
-
-      {users.map((user) => (
-        <Card key={user.id} style={{ marginBottom: "20px" }}>
-          <CardMedia component="img" height="140" image={user.photo} alt="user photo" />
-          <CardContent>
-            <div>
-              <strong>First Name: </strong> {user.fname}
-            </div>
-            <div>
-              <strong>Last Name: </strong> {user.lname}
-            </div>
-            <div>
-              <strong>Email: </strong> {user.email}
-            </div>
-            <div>
-              <strong>Role: </strong> {user.role}
-            </div>
-            <IconButton
-              onClick={() => {
-                // Your user editing logic here
-                const newData = {}; // Define your updated data
-                handleEditUser(user.id, newData);
-              }}
-            >
-              <AddIcon /> Edit
-            </IconButton>
-          </CardContent>
-        </Card>
-      ))}
+      <Grid container spacing={2}>
+        {users.map((user) => (
+          <Grid item key={user.id} xs={12} sm={4} md={2}>
+            <Card style={{ marginBottom: "20px", fontSize: 13 }}>
+              <CardMedia component="img" height="140" image={user.photo} alt="user photo" />
+              <CardContent style={{ fontSize: 13 }}>
+                <div>
+                  <strong>First Name: </strong> {user.fname}
+                </div>
+                <div>
+                  <strong>Last Name: </strong> {user.lname}
+                </div>
+                <div>
+                  <strong>Email: </strong> {user.email}
+                </div>
+              </CardContent>
+              <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", padding: "5px", paddingLeft: "13px" }}>
+                <div>
+                  <strong>Role: </strong> {user.role}
+                </div>
+                <IconButton onClick={(e) => handleCardClick(e, user)}>
+                  <MoreHorizIcon />
+                </IconButton>
+              </div>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        {selectedUser && (
+          <div>
+            <MenuItem onClick={() => handleEditUser(selectedUser.id, {})}>Edit</MenuItem>
+            <MenuItem onClick={() => handleEditUser(selectedUser.id, {})}>Delete</MenuItem>
+            <MenuItem onClick={() => handleEditUser(selectedUser.id, {})}>Disable User</MenuItem>
+            <MenuItem onClick={() => handleEditUser(selectedUser.id, {})}>View User</MenuItem>
+          </div>
+        )}
+      </Popover>
     </div>
   );
 };
