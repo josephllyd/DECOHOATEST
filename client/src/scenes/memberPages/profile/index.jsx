0a5ba46241db56f067d417d96895c5609e2798fe
josephllyd <marumasjoseph@gmail.com>
2023-11-10 00:27:00 +0800

    initial edit profile option

--- client/src/scenes/memberPages/profile/index.jsx
+++ client/src/scenes/memberPages/profile/index.jsx
@@ -1,33 +1,78 @@
-  import { useState, useEffect } from "react";
-  import { useTheme } from "@mui/material/styles";
-  import { fetchUsers, handleEditUser, useUserData, componentDidMount } from "api/usersApi";
-  import { CardMedia } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useTheme, Button, Dialog, DialogTitle, DialogContent, TextField, DialogActions } from "@mui/material";
+import { fetchUsers, handleEditUser, useUserData, componentDidMount } from "api/usersApi";
+import { CardMedia } from "@mui/material";
 
-  const Profile = () => {
-    const userData = useUserData();
-    const theme = useTheme();
-    const [users, setUsers] = useState([]);
-    const [selectedUser, setSelectedUser] = useState(userData || { photo: '', fname: '', lname: '', email: '' });
+const Profile = () => {
+  const userData = useUserData();
+  const theme = useTheme();
+  const [users, setUsers] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(userData || { photo: '', fname: '', lname: '', email: '' });
+  const [open, setOpen] = useState(false);
 
+  useEffect(() => {
+    fetchUsers(setUsers);
+  }, []);
 
-    useEffect(() => {
-      fetchUsers(setUsers);
-    }, []);
-
-
-    return (
-      <div style={{ flex: 1, padding: "20px", fontSize: "20px" }}>
-        {userData && (
-          <div style={{ fontSize: 13 }}>
-
-            <CardMedia component="img" height="140" width="50" image={userData.photo} alt="user photo" /> <br/>
-            <strong>First Name: </strong>{userData.fname} <br/>
-            <strong>Last Name: </strong> {userData.lname} <br/>
-            <strong> Email: </strong>{userData.email} <br/>
-          </div>
-        )}
-      </div>
-    );
+  const handleOpen = () => {
+    setOpen(true);
   };
 
-  export default Profile;
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleEditProfile = () => {
+    handleEditUser(selectedUser.id, selectedUser, setUsers);
+    setOpen(false);
+  };
+
+  return (
+    <div style={{ flex: 1, padding: "20px", fontSize: "20px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+      {userData && (
+        <div style={{ fontSize: 15 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            width="140"
+            image={userData.photo}
+            alt="user photo"
+            style={{ objectFit: "cover", borderRadius: "10%", marginBottom: "20px", maxWidth: "100%", display: "flex", flexDirection: "column", alignItems: "center" }}
+          />
+          <strong>First Name: </strong>{userData.fname} <br />
+          <strong>Last Name: </strong> {userData.lname} <br />
+          <strong> Email: </strong> {userData.email} <br />
+          <Button variant="outlined" onClick={handleOpen} style={{ marginTop: "20px" }}>
+            Edit
+          </Button>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Edit Details</DialogTitle>
+            <DialogContent>
+              <TextField
+                label="First Name"
+                value={selectedUser.fname}
+                onChange={(e) => setSelectedUser({ ...selectedUser, fname: e.target.value })}
+              />
+              <TextField
+                label="Last Name"
+                value={selectedUser.lname}
+                onChange={(e) => setSelectedUser({ ...selectedUser, lname: e.target.value })}
+              />
+              <TextField
+                label="Email"
+                value={selectedUser.email}
+                onChange={(e) => setSelectedUser({ ...selectedUser, email: e.target.value })}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleEditProfile}>Save</Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
