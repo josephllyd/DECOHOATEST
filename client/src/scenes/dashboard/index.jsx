b31adf8dd9cc33590b228d140a33636a2b96d6a3
josephllyd <marumasjoseph@gmail.com>
2023-12-05 00:18:15 +0800

    initial dashboard

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,4 +1,9 @@
+import { Card, CardMedia } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import resHome from "../../assets/resHomes.jpg";
+import occup from "../../assets/occup.jpg";
+import fam from "../../assets/fam.jpg";
+import amne from "../../assets/amne.jpg"
 
 const Dashboard = () => {
   const [userData, setUserData] = useState({});
@@ -39,11 +44,52 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div style={{ flex: 1, padding: "40px", fontSize: '20px' }}>
-      Welcome {userData.fname} ! <br />
     
-      <br />
+  <div>
+    <div style={{ display: "flex", flex: 1,  flexDirection: "column", paddingTop: "0px", paddingLeft: "40px", paddingRight: "40px", paddingBottom: "0px" }}>
+        <h2><b>Welcome {userData.fname} !</b></h2>
+        <h4 style={{margin: "0px"}}><b>Dashboard</b></h4>
     </div>
+    <div style={{ display: "flex", flex: 1, flexGrow: 2, flexDirection: "row" }} >
+      <div style={{ display: "flex", flex: 1, padding: "10px", flexGrow: 3,  paddingLeft: "40px", flexDirection: "column",flexWrap: "wrap", alignItems: "stretch" }}>
+        <div style={{ display: "flex", flex: 1, flexDirection: "row", padding: "10px", flexWrap: "wrap" }}>
+          <Card style={{ display: "flex", flex: 1, padding: "5px", margin: "10px", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <CardMedia component="img" height="140" src={resHome} alt="image" />
+            <p style={{margin: "0px", marginTop: "10px"}}>100 Residential Homes</p>
+          </Card>
+          <Card style={{ display: "flex", flex: 1, padding: "5px", margin: "10px", flexDirection: "column", justifyContent: "center", alignItems: "center"  }}>
+            <CardMedia component="img" height="140" image={fam} alt="image" />
+            <p style={{margin: "0px", marginTop: "10px"}}> 72 Homeowners</p>
+          </Card>
+          <Card style={{ display: "flex", flex: 1, padding: "5px", margin: "10px", flexDirection: "column", justifyContent: "center", alignItems: "center"  }}>
+            <CardMedia component="img" height="140" image={occup} alt="image" />
+            <p style={{margin: "0px", marginTop: "10px"}}>382 Total Population</p>
+          </Card>
+          <Card style={{ display: "flex", flex: 1, padding: "5px", margin: "10px", flexDirection: "column", justifyContent: "center", alignItems: "center"  }}>
+            <CardMedia component="img" height="140" image={amne} alt="image" />
+            <p style={{margin: "0px", marginTop: "10px"}}>8 Amazing Amneties</p>
+          </Card>
+        </div>
+        <div >
+          <h5>Maintenance Requests</h5>
+          <div style={{ display: "flex", flex: 1, flexDirection: "row", padding: "10px", flexWrap: "wrap" }}>
+            <CardMedia/>
+            <p>Name</p>
+            <p>Subject</p>
+            <p>Date</p>
+          </div>
+        </div>
+        <div>
+          <h5>Payment History</h5> 
+        </div>
+      </div>
+      <div style={{ display: "flex", flex: 1, padding: "10px", flexGrow: 1, paddingRight: "40px" }}>
+        <Card style={{ display: "flex", flex: 1, padding: "5px", margin: "10px", justifyContent: "center"}}>
+          <h5 style={{margin: "10px"}}>Announcements</h5>
+        </Card>
+      </div>
+    </div>  
+  </div>
   );
 };
 
